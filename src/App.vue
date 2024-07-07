79919a8424cc4b0487e6f96f694b92f85d7390f9
tingxuan0911 <candy521927@yahoo.com.tw>
2024-07-07 22:38:39 +0800

    建置

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Header from "./components/header.vue";
+</script>
+
+<template>
+  <div>
+    <Header />
+  </div>
+</template>
+
+
---
+++ src/components/header.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { ref } from 'vue'
+
+
+</script>
+
+<template>
+<div>
+  <div class="  text-teal-600">99555</div>
+</div>
+</template>
+
